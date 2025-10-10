bf6dbc46e062a2423408ae7d7feb3ecdd4e7f3d8
Isaac QS <inqs.909@gmail.com>
2025-10-09 23:32:57 -0700

    Updates

--- posts/images.R
+++ posts/images.R
@@ -11,3 +11,15 @@ p <- ggplot(
   theme_bw() +
   xlab("FLipper Length") +
   ylab("Body Mass")
+
+p <- ggplot(Melanoma, aes(thickness, dead)) +
+  geom_point(alpha = 0.7) +
+  labs(x = "Tumour thickness (mm)", y = "Dead (1=yes, 0=no)") +
+  stat_smooth(
+    method = "glm",
+    se = F,
+    method.args = list(family = "binomial"),
+    color = "blue"
+  ) +
+  theme_bw()
+ggsave(filename = "posts/img/logistic.png")
